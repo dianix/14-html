0d508c6d050aba6d2c849427c6e540dbd3bc8883
Diana <dianavazquez.dcg@gmail.com>
2017-03-16 11:06:21 -0600

    Ya funcinonagit add .! =D *en mi interior brinco por todas las paredes felizmente*

--- Ejercicio02-conversor/fahrenheit-celsius.js
+++ Ejercicio02-conversor/fahrenheit-celsius.js
@@ -1,8 +1,12 @@
-function convertirCelsius(grados){
-	return ((grados-32)*(5/9));
-	
+//alert("Hola");
+function convertirCelsius(){
+	var gradosF = document.getElementById("gradosFahrenheit").value;
+	var resultado = ((gradosF-32)*(5/9));
+	document.getElementById("gradosCelsius").value = resultado;	
 }
 
-function convertirFahrenheit(grados){
-	return (grados * 1.8 + 32);
+function convertirFahrenheit(){
+	var gradosC = document.getElementById("gradosCelsius").value;
+	var resultado = gradosC * 1.8 + 32;
+	document.getElementById("gradosFahrenheit").value = resultado;		
 }
---
+++ ejercicio03-ejemplo/calcularArea.js
@@ -0,0 +1,6 @@
+alert("hola");
+function calcularArea(){
+	var area = document.getElementById("area").value;
+	var resultado = Math.PI * parseInt(area);
+	document.getElementById("resultadoArea").value = resultado;
+}
